d80f3faf177029221d2c4e7d5061cff7f9144ba7
Navneet <navneetkumar.symfony813@webkul.in>
2024-10-18 15:05:02 +0530

    Added lock file mechanism and file ownership update in web-entrypoint.sh

--- dockerfiles/web-entrypoint.sh
+++ dockerfiles/web-entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-composer install
-npm install
-php artisan unopim:install -n
+LOCK_FILE="/var/lock/unopim.lock"
+
+if [ ! -f "$LOCK_FILE" ]; then
+    composer install
+    npm install
+    php artisan unopim:install -n
+    
+    touch "$LOCK_FILE"
+fi
+
+chown -R 1001:1001 /var/www/html
 
 # Hand back control to the default entrypoint
 apache2-foreground
