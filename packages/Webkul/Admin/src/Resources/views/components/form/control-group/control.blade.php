81531aa6f6b1705ecf97ffaf977616d4094447d8
vipin-yadav-webkul <vipinyadav.akeneo432@webkul.in>
2024-11-26 19:14:11 +0530

    update control.blade.php for add select and remove event and select type attribute

--- packages/Webkul/Admin/src/Resources/views/components/form/control-group/control.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/form/control-group/control.blade.php
@@ -380,6 +380,8 @@
                 :name="name"
                 v-model="selectedValue"
                 v-bind="field"
+                @select="selectOption"
+                @remove="removeOption"
             >
 
             </v-multiselect>   
@@ -462,7 +464,21 @@
                     } catch (error) {
                         return this.value;
                     }
-                }
+                },
+                selectOption(newValue) {
+                    this.$emit('select-option', {
+                        target: {
+                            value: newValue
+                        }
+                    });
+                },
+                removeOption(removeValue) {
+                    this.$emit('remove-option', {
+                        target: {
+                            value: removeValue
+                        }
+                    });
+                },
             }
         });
     </script>
@@ -484,6 +500,8 @@
                 :name="name"
                 v-model="selectedValue"
                 v-bind="field"
+                @select="selectOption"
+                @remove="removeOption"
             >
 
             </v-multiselect>   
@@ -567,7 +585,21 @@
                     } catch (error) {
                         return this.value;
                     }
-                }
+                },
+                selectOption(newValue) {
+                    this.$emit('select-option', {
+                        target: {
+                            value: newValue
+                        }
+                    });
+                },
+                removeOption(removeValue) {
+                    this.$emit('remove-option', {
+                        target: {
+                            value: removeValue
+                        }
+                    });
+                },
             }
         });
     </script>
