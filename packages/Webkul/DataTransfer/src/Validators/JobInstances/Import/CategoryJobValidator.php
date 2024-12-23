71beca45817ef6c94914e8b55231c345cc75b4b0
Pravat Senapati <pravat.senapati735@webkul.in>
2024-12-23 11:35:28 +0530

    feat: add job validator

---
+++ packages/Webkul/DataTransfer/src/Validators/JobInstances/Import/CategoryJobValidator.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Webkul\DataTransfer\Validators\JobInstances\Import;
+
+use Webkul\DataTransfer\Validators\JobInstances\Default\JobValidator;
+use Webkul\DataTransfer\Rules\SeparatorTypes;
+
+class CategoryJobValidator extends JobValidator
+{
+    /**
+     * Validation rules for job instance
+     */
+    public function getRules($option): array
+    {
+        if (isset($option['id'])) {
+            $this->rules['file'] = 'mimes:csv,xls,xlsx,txt';
+        }
+        $this->rules['field_separator'] = ['required', new SeparatorTypes];
+
+        return $this->rules;
+    }
+
+    /**
+     * Stores validation rules for data
+     */
+    protected array $rules = [
+        'action'              => 'required:in:append,delete',
+        'validation_strategy' => 'required:in:stop-on-errors,skip-errors',
+        'allowed_errors'      => 'required|integer|min:0',
+        'file'                => 'required|mimes:csv,xls,xlsx,txt',
+    ];
+
+    /**
+     * Names to be used for attributes during generation of error message
+     */
+    protected array $attributeNames = [
+        'action'               => 'Action',
+        'validation_strategy'  => 'Validation Strategy',
+        'allowed_errors'       => 'Allowed Errors',
+        'field_separator'      => 'Field Separator',
+        'file'                 => 'File',
+    ];
+}
