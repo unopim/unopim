b8ac945500dd1af0b4bdcb9f6f8c1c8efe180713
vipin-yadav-webkul <vipinyadav.akeneo432@webkul.in>
2024-12-27 11:26:59 +0530

    Update notification EventServiceProvider parent class

--- packages/Webkul/Notification/src/Providers/EventServiceProvider.php
+++ packages/Webkul/Notification/src/Providers/EventServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace Webkul\Notification\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
