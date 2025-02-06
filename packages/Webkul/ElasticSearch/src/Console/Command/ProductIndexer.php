9df31cff95c22d985a88e83fa3f8c58b7fbc4775
devansh.pal507 <devansh.pal507@webkul.in>
2025-02-06 17:59:30 +0530

    fix: use bulk api for elastic in product indexer

--- packages/Webkul/ElasticSearch/src/Console/Command/ProductIndexer.php
+++ packages/Webkul/ElasticSearch/src/Console/Command/ProductIndexer.php
@@ -12,6 +12,8 @@ use Webkul\Product\Models\Product;
 
 class ProductIndexer extends Command
 {
+    const BATCH_SIZE = 10000;
+
     protected $signature = 'product:index';
 
     protected $description = 'Index all products into Elasticsearch';
@@ -28,85 +30,11 @@ class ProductIndexer extends Command
 
             $start = microtime(true);
 
-            $products = DB::table('products')->get();
-
             $productIndex = strtolower($indexPrefix.'_products');
 
-            if ($products->isNotEmpty()) {
-                $elasticProduct = $this->getProductUpdates($productIndex, $this);
+            $totalProducts = DB::table('products')->count();
 
-                $this->info('Indexing products into Elasticsearch...');
-                $dbProductIds = $products->pluck('id')->toArray();
-
-                $progressBar = new ProgressBar($this->output, count($products));
-                $progressBar->start();
-
-                foreach ($products as $productDB) {
-                    $product = new Product;
-
-                    $product->forceFill((array) $productDB);
-                    $product->syncOriginal();
-                    if (
-                        (
-                            isset($elasticProduct[$product->id])
-                            && $elasticProduct[$product->id] != Carbon::parse($product->updated_at)->setTimezone('UTC')->format('Y-m-d\TH:i:s.u\Z')
-                        )
-                        || ! isset($elasticProduct[$product->id])
-                    ) {
-                        Elasticsearch::index([
-                            'index' => $productIndex,
-                            'id'    => $product->id,
-                            'body'  => $product->toArray(),
-                        ]);
-                    }
-                    $progressBar->advance();
-                }
-
-                $progressBar->finish();
-                $this->newLine();
-                $this->info('Product indexing completed.');
-
-                Log::channel('elasticsearch')->info('Product indexing completed.');
-
-                $this->info('Checking for stale products to delete...');
-
-                $elasticProductIds = collect(Elasticsearch::search([
-                    'index' => $productIndex,
-                    'body'  => [
-                        '_source' => false,
-                        'query'   => [
-                            'match_all' => new \stdClass,
-                        ],
-                        'size' => 1000000000,
-                    ],
-                ])['hits']['hits'])->pluck('_id')->map(fn ($id) => (int) $id)->toArray();
-
-                $productsToDelete = array_diff($elasticProductIds, $dbProductIds);
-
-                if (! empty($productsToDelete)) {
-                    $this->info('Deleting stale products from Elasticsearch...');
-                    $deleteProgressBar = new ProgressBar($this->output, count($productsToDelete));
-                    $deleteProgressBar->start();
-
-                    foreach ($productsToDelete as $productId) {
-                        Elasticsearch::delete([
-                            'index' => $productIndex,
-                            'id'    => $productId,
-                        ]);
-                        $deleteProgressBar->advance();
-                    }
-
-                    $deleteProgressBar->finish();
-                    $this->newLine();
-                    $this->info('Stale products deleted successfully.');
-
-                    Log::channel('elasticsearch')->info('Stale products deleted successfully.');
-                } else {
-                    $this->info('No stale products to delete.');
-
-                    Log::channel('elasticsearch')->info('No stale products to delete.');
-                }
-            } else {
+            if ($totalProducts === 0) {
                 $this->info('No product found in the database. Attempting to delete the index if it exists:-');
                 Log::channel('elasticsearch')->info('No product found in the database. Attempting to delete the index if it exists:-');
 
@@ -130,6 +58,119 @@ class ProductIndexer extends Command
                         throw $e;
                     }
                 }
+
+                return;
+            }
+
+            $progressBar = new ProgressBar($this->output, $totalProducts);
+
+            $dbProductIds = [];
+            $elasticProduct = $this->getProductUpdates($productIndex, $this);
+
+            for ($offset = 0; $offset < $totalProducts; $offset += self::BATCH_SIZE) {
+                $products = DB::table('products')->offset($offset)->limit(self::BATCH_SIZE)->get();
+
+                if ($products->isNotEmpty()) {
+                    if ($offset === 0) {
+                        $this->info('Indexing products into Elasticsearch...');
+
+                        $progressBar->start();
+                    }
+
+                    $productsToUpdate = [];
+
+                    foreach ($products as $productDB) {
+                        $product = new Product;
+
+                        $product->forceFill((array) $productDB);
+                        $product->syncOriginal();
+
+                        $productId = $product->id;
+
+                        $dbProductIds[] = $productId;
+
+                        if (
+                            (
+                                isset($elasticProduct[$productId])
+                                && $elasticProduct[$productId] != Carbon::parse($product->updated_at)->setTimezone('UTC')->format('Y-m-d\TH:i:s.u\Z')
+                            )
+                            || ! isset($elasticProduct[$productId])
+                        ) {
+                            $productsToUpdate['body'][] = [
+                                'index' => [
+                                    '_index' => $productIndex,
+                                    '_id'    => $productId,
+                                ],
+                            ];
+
+                            $productsToUpdate['body'][] = $product->toArray();
+                        }
+
+                        $progressBar->advance();
+                    }
+
+                    if ($productsToUpdate) {
+                        Elasticsearch::bulk($productsToUpdate);
+                    }
+                }
+            }
+
+            $progressBar->finish();
+            $this->newLine();
+            $this->info('Product indexing completed.');
+
+            Log::channel('elasticsearch')->info('Product indexing completed.');
+
+            $this->info('Checking for stale products to delete...');
+
+            $elasticProductIds = collect(Elasticsearch::search([
+                'index' => $productIndex,
+                'body'  => [
+                    '_source' => false,
+                    'query'   => [
+                        'match_all' => new \stdClass,
+                    ],
+                    'size' => 1000000000,
+                ],
+            ])['hits']['hits'])->pluck('_id')->map(fn ($id) => (int) $id)->toArray();
+
+            $productsToDelete = array_diff($elasticProductIds, $dbProductIds);
+
+            if (! empty($productsToDelete)) {
+                $this->info('Deleting stale products from Elasticsearch...');
+                $deleteProgressBar = new ProgressBar($this->output, count($productsToDelete));
+                $deleteProgressBar->start();
+
+                $productChunks = array_chunk($productsToDelete, self::BATCH_SIZE);
+
+                foreach ($productChunks as $chunk) {
+                    $deleteProducts = [];
+
+                    foreach ($chunk as $productId) {
+                        $deleteProducts['body'][] = [
+                            'delete' => [
+                                '_index' => $productIndex,
+                                '_id'    => $productId,
+                            ],
+                        ];
+
+                        $deleteProgressBar->advance();
+                    }
+
+                    if ($deleteProducts) {
+                        Elasticsearch::bulk($deleteProducts);
+                    }
+                }
+
+                $deleteProgressBar->finish();
+                $this->newLine();
+                $this->info('Stale products deleted successfully.');
+
+                Log::channel('elasticsearch')->info('Stale products deleted successfully.');
+            } else {
+                $this->info('No stale products to delete.');
+
+                Log::channel('elasticsearch')->info('No stale products to delete.');
             }
 
             $end = microtime(true);
@@ -190,7 +231,6 @@ class ProductIndexer extends Command
                     'error' => $e->getMessage(),
                 ]);
             }
-
         } catch (\Exception $e) {
             if (str_contains($e->getMessage(), 'index_not_found_exception')) {
                 if ($command) {
