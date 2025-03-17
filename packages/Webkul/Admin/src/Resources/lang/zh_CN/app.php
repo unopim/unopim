898e01e79e06cc650b6ebbe1686897385af56b42
vipin-yadav-webkul <vipinyadav.akeneo432@webkul.in>
2025-03-17 13:25:59 +0530

    run pint command

--- packages/Webkul/Admin/src/Resources/lang/zh_CN/app.php
+++ packages/Webkul/Admin/src/Resources/lang/zh_CN/app.php
@@ -1646,11 +1646,11 @@ return [
             ],
 
             'toolbar' => [
-                'length-of' => '：length 的',
+                'length-of'  => '：length 的',
                 '的'         => '的',
-                'per-page'  => '每页',
-                'results'   => '：total 结果',
-                'selected'  => '：total 选定',
+                'per-page'   => '每页',
+                'results'    => '：total 结果',
+                'selected'   => '：total 选定',
 
                 'mass-actions' => [
                     'submit'        => '提交',
