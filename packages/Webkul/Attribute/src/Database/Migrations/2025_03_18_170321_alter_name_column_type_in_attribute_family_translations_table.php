9549e997b43eeb9f52f84b9aa9be0a1fa36ccdc1
devansh.pal507 <devansh.pal507@webkul.in>
2025-03-18 18:58:12 +0530

    perf: alter column type from text to string
    - changed for the following tables
    
    * category_field_translations
    * category_field_option_translations
    * attribute_family_translations
    * attribute_group_translations
    * attribute_option_translations
    * attribute_translations

---
+++ packages/Webkul/Attribute/src/Database/Migrations/2025_03_18_170321_alter_name_column_type_in_attribute_family_translations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('attribute_family_translations', function (Blueprint $table) {
+            $table->string('name', 255)->nullable()->index('attribute_family_translations_name')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('attribute_family_translations', function (Blueprint $table) {
+            $table->dropIndex('name');
+
+            $table->text('name')->nullable()->change();
+        });
+    }
+};
---
+++ packages/Webkul/Category/src/Database/Migrations/2025_03_18_135047_alter_label_column_type_in_category_field_option_translations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('category_field_option_translations', function (Blueprint $table) {
+            $table->string('label', 255)->nullable()->index('category_field_option_translations_label')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('category_field_option_translations', function (Blueprint $table) {
+            $table->dropIndex('label');
+
+            $table->text('label')->nullable()->change();
+        });
+    }
+};
