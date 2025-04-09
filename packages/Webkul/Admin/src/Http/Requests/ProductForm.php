e3fd8b4bf3b55f8d566b2d03e22807d5da7e5a8d
devansh.pal507 <devansh.pal507@webkul.in>
2025-04-09 18:21:29 +0530

    refactor: remove unique validation from request class handling with validator in controller

--- packages/Webkul/Admin/src/Http/Requests/ProductForm.php
+++ packages/Webkul/Admin/src/Http/Requests/ProductForm.php
@@ -2,30 +2,11 @@
 
 namespace Webkul\Admin\Http\Requests;
 
-use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\ValidationException;
 use Webkul\Core\Rules\Slug;
-use Webkul\Product\Repositories\ProductRepository;
 
 class ProductForm extends FormRequest
 {
-    /**
-     * Rules.
-     *
-     * @var array
-     */
-    protected $rules;
-
-    /**
-     * Create a new form request instance.
-     *
-     * @return void
-     */
-    public function __construct(
-        protected ProductRepository $productRepository
-    ) {}
-
     /**
      * Determine if the product is authorized to make this request.
      *
@@ -43,25 +24,9 @@ class ProductForm extends FormRequest
      */
     public function rules()
     {
-        $product = $this->productRepository->find($this->id);
-
-        $this->rules = $product->getTypeInstance()->getTypeValidationRules();
-
-        if ($this->uniqueFields) {
-            foreach ($this->uniqueFields as $fieldName => $fieldNamespace) {
-                $pathToValue = str_replace(['values', '[', '][', ']'], ['', '->', '->', ''], $fieldNamespace);
-
-                $this->rules[$fieldName] = 'unique:products,values'.$pathToValue;
-
-                if ($this->id) {
-                    $this->rules[$fieldName] = $this->rules[$fieldName].",{$this->id}";
-                }
-            }
-        }
-
-        $this->rules['sku'] = ['required', 'unique:products,sku,'.$this->id, new Slug];
-
-        return $this->rules;
+        return [
+            'sku' => ['required', 'unique:products,sku,'.$this->id, new Slug],
+        ];
     }
 
     public function prepareForValidation()
@@ -70,42 +35,6 @@ class ProductForm extends FormRequest
             $this->merge([
                 'sku' => $this->values['common']['sku'],
             ]);
-        }
-    }
-
-    /**
-     * Handle a failed validation attempt.
-     *
-     * @throws ValidationException
-     */
-    protected function failedValidation(Validator $validator): void
-    {
-        try {
-            parent::failedValidation($validator);
-        } catch (ValidationException $e) {
-            $messages = [];
-
-            $translator = $validator->getTranslator();
-
-            foreach ($validator->errors()->messages() as $key => $message) {
-                if (
-                    is_string($key)
-                    && str_contains($key, 'values')
-                    && isset($this->uniqueFields[$key])
-                ) {
-                    $messages[$this->uniqueFields[$key]] = $translator->get('admin::app.catalog.products.unique-validation');
-                }
-
-                $messages[$key] = $message;
-            }
-
-            $e = $e::withMessages($messages);
-
-            if (count($messages) == 1 && isset($messages['sku'][0])) {
-                session()->flash('error', $messages['sku'][0]);
-            }
-
-            throw $e;
         }
     }
 }
