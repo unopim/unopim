b63c1ecc1a108ff530d32977245f227d1cca3d9c
dripargupta.akeneo <dripargupta.akeneo@webkul.in>
2025-05-09 13:13:10 +0530

    fix: validation on 255 character limit in code input

--- packages/Webkul/Core/src/Rules/Code.php
+++ packages/Webkul/Core/src/Rules/Code.php
@@ -12,6 +12,10 @@ class Code implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
+        if (strlen($value) > 255) {
+            $fail('validation.max.string')->translate(['attribute' => $attribute, 'max' => 255]);
+        }
+
         if (! preg_match('/^[a-zA-Z0-9_]+$/', $value)) {
             $fail('core::validation.code')->translate();
         }
