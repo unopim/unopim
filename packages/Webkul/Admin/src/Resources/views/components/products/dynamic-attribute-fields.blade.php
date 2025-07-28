72e929b53c863a8ef262579188bc6bdcf7e4fad6
Ravindra Kumar Yadav <ravindra.yadav322@webkul.in>
2025-07-28 11:48:41 +0530

    Fix: Fixed alignment

--- packages/Webkul/Admin/src/Resources/views/components/products/dynamic-attribute-fields.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/products/dynamic-attribute-fields.blade.php
@@ -126,196 +126,204 @@
     {!! view_render_event('unopim.admin.products.dynamic-attribute-fields.control.'.$fieldType.'.before', ['field' => $field, 'value' => $value, 'fieldName' => $fieldName]) !!}
 
     @switch ($fieldType)
-    @case ('checkbox')
-    @if (! empty($value))
-    <input type="hidden" name="{{ $fieldName }}" value="">
-    @endIf
+        @case ('checkbox')
+            @if (! empty($value))
+                <input type="hidden" name="{{ $fieldName }}" value="">
+            @endIf
 
-    @php
-    $fieldName = $fieldName.'[]';
+            @php
+                $fieldName = $fieldName.'[]';
 
-    $selectedValue = ! empty($value) && is_string($value) ? explode(',', $value) : $value;
+                $selectedValue = ! empty($value) && is_string($value) ? explode(',', $value) : $value;
 
-    $selectedValue = empty($selectedValue) || ! is_array($selectedValue) ? [] : $selectedValue;
-    @endphp
+                $selectedValue = empty($selectedValue) || ! is_array($selectedValue) ? [] : $selectedValue;
+            @endphp
 
-    @foreach ($field->options as $option)
-    <div class="flex py-2 items-center gap-2">
-        <x-admin::form.control-group.control
-            type="checkbox"
-            :id="$field->code . '_' . $option->id"
-            :name="$fieldName"
-            :value="$option->code"
-            ::rules="{{ $field->getValidationsField() }}"
-            :label="$fieldLabel"
-            :for="$field->code . '_' . $option->id"
-            :checked="(bool) false !== array_search($option->code, $selectedValue)" />
+            @foreach ($field->options as $option)
+                <div class="flex py-2 items-center gap-2">
+                    <x-admin::form.control-group.control
+                        type="checkbox"
+                        :id="$field->code . '_' . $option->id"
+                        :name="$fieldName"
+                        :value="$option->code"
+                        ::rules="{{ $field->getValidationsField() }}"
+                        :label="$fieldLabel"
+                        :for="$field->code . '_' . $option->id"
+                        :checked="(bool) false !== array_search($option->code, $selectedValue)" 
+                    />
 
-        <label
-            class="text-xs text-gray-600 dark:text-gray-300 font-medium cursor-pointer select-none"
-            for="{{ $field->code . '_' . $option->id }}">
-            {{ $option->translate($currentLocaleCode)?->label ?? "[{$option->code}]" }}
-        </label>
-    </div>
-    @endforeach
+                    <label
+                        class="text-xs text-gray-600 dark:text-gray-300 font-medium cursor-pointer select-none"
+                        for="{{ $field->code . '_' . $option->id }}"
+                    >
+                        {{ $option->translate($currentLocaleCode)?->label ?? "[{$option->code}]" }}
+                    </label>
+                </div>
+            @endforeach
 
-    @break
-    @case ('boolean')
-    <input type="hidden" name="{{ $fieldName }}" value="false" />
+            @break
+        @case ('boolean')
+            <input type="hidden" name="{{ $fieldName }}" value="false" />
 
-    <x-admin::form.control-group.control
-        type="switch"
-        :id="$field->code"
-        :name="$fieldName"
-        :label="$fieldLabel"
-        :checked="(bool) ('true' == strtolower($value))"
-        value="true" />
-
-    @break
-    @case('image')
-    @php
-    if (is_array($value)) {
-    $value = current($value);
-    }
-
-    $savedImage = ! empty($value) ? [
-    'id' => 0,
-    'url' => Storage::url($value),
-    'value' => $value,
-    ] : [];
-    @endphp
-
-    @if (! empty($value))
-    <!-- Emoty value sent when value is deleted need to send empty value for this field -->
-    <input type="hidden" name="{{ $fieldName }}" value="">
-    @endIf
-
-    <x-admin::media.images
-        name="{{ $fieldName }}"
-        ::class="[errors && errors['{{ $fieldName }}'] ? 'border !border-red-600 hover:border-red-600' : '']"
-        :id="$field->code"
-        ::rules="{{ $field->getValidationsField() }}"
-        :uploaded-images="! empty($value) ? [$savedImage] : []"
-        width='210px' />
-    @break
-    @case('gallery')
-    @php
-    $savedImages = ! empty($value) ? array_map(function ($image, $index) {
-    return [
-    'id' => uniqid(),
-    'url' => Storage::url($image),
-    'value' => $image,
-    ];
-    }, (array)$value, array_keys((array)$value)) : [];
-    @endphp
-
-    @if (! empty($value))
-    <!-- Empty value sent when value is deleted need to send empty value for this field -->
-    <input type="hidden" name="{{ $fieldName }}" value="">
-    @endIf
-
-    <x-admin::media.images
-        name="{{ $fieldName }}"
-        ::class="[errors && errors['{{ $fieldName }}'] ? 'border !border-red-600 hover:border-red-600' : '']"
-        :id="$field->code"
-        ::rules="{{ $field->getValidationsField() }}"
-        :uploaded-images="! empty($value) ? $savedImages : []"
-        :allow-multiple=true
-        width='210px' />
-    @break
-    @case('file')
-    @php
-    $fileName = last(explode('/', $value));
-    $fileName = strlen($fileName) > 20 ? substr($fileName, 0, 20) . '...' : $fileName;
-
-    $savedFile = ! empty($value) ? [
-    'id' => 0,
-    'url' => Storage::url($value),
-    'value' => $value,
-    'fileName' => $fileName,
-    ] : [];
-    @endphp
-
-    @if (! empty($value))
-    <!--  Emoty value sent when value is deleted need to send empty value for this field -->
-    <input type="hidden" name="{{ $fieldName }}" value="">
-    @endIf
-
-    <x-admin::media.files
-        type="video"
-        :id="$field->code"
-        :name="$fieldName"
-        ::rules="{{ $field->getValidationsField() }}"
-        :label="$fieldLabel"
-        :uploaded-files="! empty($value) ? [$savedFile] : []"
-        value="{{$value}}"
-        class="mt-3" />
-    @break
-    @case('price')
-    @php
-    $value = !is_array($value) && !empty($value) ? json_decode($value, true) : $value;
-    @endphp
-    <div class="grid gap-4 [grid-template-columns:repeat(auto-fit,_minmax(200px,_1fr))]">
-        @foreach ($channelCurrencies as $currency)
-        @php $currencyValue = $value[$currency->code] ?? ''; @endphp
-        <div class="grid w-full">
             <x-admin::form.control-group.control
-                type="price"
+                type="switch"
                 :id="$field->code"
-                :name="$fieldName . '[' . $currency->code . ']'"
+                :name="$fieldName"
+                :label="$fieldLabel"
+                :checked="(bool) ('true' == strtolower($value))"
+                value="true" 
+            />
+
+            @break
+        @case('image')
+            @php
+                if (is_array($value)) {
+                    $value = current($value);
+                }
+
+                $savedImage = ! empty($value) ? [
+                    'id' => 0,
+                    'url' => Storage::url($value),
+                    'value' => $value,
+                ] : [];
+            @endphp
+
+            @if (! empty($value))
+                <!-- Emoty value sent when value is deleted need to send empty value for this field -->
+                <input type="hidden" name="{{ $fieldName }}" value="">
+            @endIf
+
+            <x-admin::media.images
+                name="{{ $fieldName }}"
+                ::class="[errors && errors['{{ $fieldName }}'] ? 'border !border-red-600 hover:border-red-600' : '']"
+                :id="$field->code"
                 ::rules="{{ $field->getValidationsField() }}"
-                :value="$currencyValue"
-                :label="$fieldLabel">
-                <x-slot:currency class="dark:text-gray-300">
-                    {{ core()->currencySymbol($currency->code) }}
-                    </x-slot>
-            </x-admin::form.control-group.control>
-            <x-admin::form.control-group.error :control-name="$fieldName . '[' . $currency->code . ']'" />
-        </div>
-        @endForeach
-    </div>
-    @break
-    @case('multiselect')
-    <!-- NO BREAK -->
-    @php
-    $value = str_contains($value, ',')
-    ? explode(',', $value)
-    : (empty($value) ? '' : [$value]);
-    @endphp
-    @case('select')
-    <!-- NO BREAK -->
-    @php
-    $selectedValue = [];
-    foreach ($field->options->whereIn('code', $value) as $option) {
-    $translatedOptionLabel = $option->translate($currentLocaleCode)?->label;
+                :uploaded-images="! empty($value) ? [$savedImage] : []"
+                width='210px' 
+            />
+            @break
+        @case('gallery')
+            @php
+                $savedImages = ! empty($value) ? array_map(function ($image, $index) {
+                    return [
+                    'id' => uniqid(),
+                    'url' => Storage::url($image),
+                    'value' => $image,
+                    ];
+                }, (array)$value, array_keys((array)$value)) : [];
+            @endphp
 
-    $selectedValue[] = [
-    'id' => $option->id,
-    'code' => $option->code,
-    'label' => ! empty($translatedOptionLabel) ? $translatedOptionLabel : "[{$option->code}]",
-    ];
-    }
+            @if (! empty($value))
+                <!-- Empty value sent when value is deleted need to send empty value for this field -->
+                <input type="hidden" name="{{ $fieldName }}" value="">
+            @endIf
 
-    if ('select' == $fieldType) {
-    $selectedValue = ! empty($selectedValue[0]) ? $selectedValue[0] : $selectedValue;
-    }
+            <x-admin::media.images
+                name="{{ $fieldName }}"
+                ::class="[errors && errors['{{ $fieldName }}'] ? 'border !border-red-600 hover:border-red-600' : '']"
+                :id="$field->code"
+                ::rules="{{ $field->getValidationsField() }}"
+                :uploaded-images="! empty($value) ? $savedImages : []"
+                :allow-multiple=true
+                width='210px'
+            />
+            @break
+        @case('file')
+            @php
+                $fileName = last(explode('/', $value));
+                $fileName = strlen($fileName) > 20 ? substr($fileName, 0, 20) . '...' : $fileName;
 
-    $value = ! empty($selectedValue) ? json_encode($selectedValue) : '';
-    @endphp
-    @default
-    <x-admin::form.control-group.control
-        :type="$fieldType"
-        :id="$field->code"
-        :name="$fieldName"
-        ::rules="{{ $field->getValidationsField() }}"
-        :tinymce="(bool) $field->enable_wysiwyg"
-        :options="json_encode([])"
-        :label="$fieldLabel"
-        :value="$value"
-        track-by="code"
-        async="true"
-        entity-name="attribute"
-        :attribute-id="$field->id" />
+                $savedFile = ! empty($value) ? [
+                    'id' => 0,
+                    'url' => Storage::url($value),
+                    'value' => $value,
+                    'fileName' => $fileName,
+                ] : [];
+            @endphp
+
+            @if (! empty($value))
+                <!--  Emoty value sent when value is deleted need to send empty value for this field -->
+                <input type="hidden" name="{{ $fieldName }}" value="">
+            @endIf
+
+            <x-admin::media.files
+                type="video"
+                :id="$field->code"
+                :name="$fieldName"
+                ::rules="{{ $field->getValidationsField() }}"
+                :label="$fieldLabel"
+                :uploaded-files="! empty($value) ? [$savedFile] : []"
+                value="{{$value}}"
+                class="mt-3" 
+            />
+            @break
+        @case('price')
+            @php
+                $value = !is_array($value) && !empty($value) ? json_decode($value, true) : $value;
+            @endphp
+            <div class="grid gap-4 [grid-template-columns:repeat(auto-fit,_minmax(200px,_1fr))]">
+                @foreach ($channelCurrencies as $currency)
+                    @php $currencyValue = $value[$currency->code] ?? ''; @endphp
+                    <div class="grid w-full">
+                        <x-admin::form.control-group.control
+                            type="price"
+                            :id="$field->code"
+                            :name="$fieldName . '[' . $currency->code . ']'"
+                            ::rules="{{ $field->getValidationsField() }}"
+                            :value="$currencyValue"
+                            :label="$fieldLabel"
+                        >
+                            <x-slot:currency class="dark:text-gray-300">
+                                {{ core()->currencySymbol($currency->code) }}
+                            </x-slot>
+                        </x-admin::form.control-group.control>
+                        <x-admin::form.control-group.error :control-name="$fieldName . '[' . $currency->code . ']'" />
+                    </div>
+                @endForeach
+            </div>
+        @break
+        @case('multiselect')
+            <!-- NO BREAK -->
+            @php
+                $value = str_contains($value, ',')
+                    ? explode(',', $value)
+                    : (empty($value) ? '' : [$value]);
+            @endphp
+        @case('select')
+            <!-- NO BREAK -->
+            @php
+                $selectedValue = [];
+                foreach ($field->options->whereIn('code', $value) as $option) {
+                    $translatedOptionLabel = $option->translate($currentLocaleCode)?->label;
+
+                    $selectedValue[] = [
+                        'id' => $option->id,
+                        'code' => $option->code,
+                        'label' => ! empty($translatedOptionLabel) ? $translatedOptionLabel : "[{$option->code}]",
+                    ];
+                }
+
+                if ('select' == $fieldType) {
+                    $selectedValue = ! empty($selectedValue[0]) ? $selectedValue[0] : $selectedValue;
+                }
+
+                $value = ! empty($selectedValue) ? json_encode($selectedValue) : '';
+            @endphp
+        @default
+            <x-admin::form.control-group.control
+                :type="$fieldType"
+                :id="$field->code"
+                :name="$fieldName"
+                ::rules="{{ $field->getValidationsField() }}"
+                :tinymce="(bool) $field->enable_wysiwyg"
+                :options="json_encode([])"
+                :label="$fieldLabel"
+                :value="$value"
+                track-by="code"
+                async="true"
+                entity-name="attribute"
+                :attribute-id="$field->id" 
+            />
     @endswitch
 
     @php
