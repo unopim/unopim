ee78eb97fabfcd57a0d9edad96c85b52d646984d
devansh.pal507 <devansh.pal507@webkul.in>
2025-08-14 18:11:50 +0530

    test: add acl permission test cases for mass actions

--- packages/Webkul/Admin/tests/Feature/Acl/Settings/CurrencyAclTest.php
+++ packages/Webkul/Admin/tests/Feature/Acl/Settings/CurrencyAclTest.php
@@ -57,3 +57,67 @@ it('should be able to delete currency if have permission', function () {
         'id' => $currency->id,
     ]);
 });
+
+it('should not be able to mass update currencies if does not have permission', function () {
+    $this->loginWithPermissions(permissions: ['settings', 'settings.currencies']);
+
+    $currencyIds = Currency::where('status', 0)->limit(2)->pluck('id')->toArray();
+
+    $this->post(route('admin.settings.currencies.mass_update'), [
+        'indices' => $currencyIds,
+        'value'   => 1,
+    ])
+        ->assertSeeText('Unauthorized');
+
+    foreach ($currencyIds as $id) {
+        $this->assertDatabaseHas($this->getFullTableName(Currency::class), ['id' => $id, 'status' => 0]);
+    }
+});
+
+it('should be able to mass update currencies if has permission', function () {
+    $this->loginWithPermissions(permissions: ['settings', 'settings.currencies', 'settings.currencies.mass_update']);
+
+    $currencyIds = Currency::where('status', 0)->limit(2)->pluck('id')->toArray();
+
+    $this->post(route('admin.settings.currencies.mass_update'), [
+        'indices' => $currencyIds,
+        'value'   => 1,
+    ])
+        ->assertOk()
+        ->assertJsonFragment(['message' => trans('admin::app.settings.currencies.index.update-success')]);
+
+    foreach ($currencyIds as $id) {
+        $this->assertDatabaseHas($this->getFullTableName(Currency::class), ['id' => $id, 'status' => 1]);
+    }
+});
+
+it('should not be able to mass delete currencies if does not have permission', function () {
+    $this->loginWithPermissions(permissions: ['settings', 'settings.currencies', 'settings.currencies.delete']);
+
+    $currencyIds = Currency::where('status', 0)->limit(2)->pluck('id')->toArray();
+
+    $this->post(route('admin.settings.currencies.mass_delete'), [
+        'indices' => $currencyIds,
+    ])
+        ->assertSeeText('Unauthorized');
+
+    foreach ($currencyIds as $id) {
+        $this->assertDatabaseHas($this->getFullTableName(Currency::class), ['id' => $id]);
+    }
+});
+
+it('should be able to mass delete currencies if has permission', function () {
+    $this->loginWithPermissions(permissions: ['settings', 'settings.currencies', 'settings.currencies.mass_delete']);
+
+    $currencyIds = Currency::where('status', 0)->limit(2)->pluck('id')->toArray();
+
+    $this->post(route('admin.settings.currencies.mass_delete'), [
+        'indices' => $currencyIds,
+    ])
+        ->assertOk()
+        ->assertJsonFragment(['message' => trans('admin::app.settings.currencies.index.delete-success')]);
+
+    foreach ($currencyIds as $id) {
+        $this->assertDatabaseMissing($this->getFullTableName(Currency::class), ['id' => $id]);
+    }
+});
