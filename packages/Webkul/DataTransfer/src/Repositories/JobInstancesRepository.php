919d5f8357a58d330632184d5a053f06ae8e8565
MD-Nurullah-Webkul <mdnurullah.mg763@webkul.com>
2025-08-22 18:23:22 +0530

    fixed category export job profile update

--- packages/Webkul/DataTransfer/src/Repositories/JobInstancesRepository.php
+++ packages/Webkul/DataTransfer/src/Repositories/JobInstancesRepository.php
@@ -4,6 +4,7 @@ namespace Webkul\DataTransfer\Repositories;
 
 use Webkul\Core\Eloquent\Repository;
 use Webkul\DataTransfer\Contracts\JobInstances;
+use Illuminate\Support\Facades\DB;
 
 class JobInstancesRepository extends Repository
 {
@@ -13,5 +14,37 @@ class JobInstancesRepository extends Repository
     public function model(): string
     {
         return JobInstances::class;
+    }
+
+    /**
+     * Update job instance with DB-driver specific fixes.
+     */
+    public function update(array $data, $id)
+    {
+        $driver = DB::getDriverName();
+
+        if ($driver === 'pgsql') {
+            if (isset($data['allowed_errors']) && $data['allowed_errors'] === '') {
+                $data['allowed_errors'] = 0;
+            }
+        }
+
+        return parent::update($data, $id);
+    }
+
+    /**
+     * Create job instance with DB-driver specific fixes.
+     */
+    public function create(array $data)
+    {
+        $driver = DB::getDriverName();
+
+        if ($driver === 'pgsql') {
+            if (isset($data['allowed_errors']) && $data['allowed_errors'] === '') {
+                $data['allowed_errors'] = 0;
+            }
+        }
+
+        return parent::create($data);
     }
 }
