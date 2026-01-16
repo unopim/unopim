578726204aa95ba855728109c1249367e59a982a
Pawan <pawannigam.qa322@webkul.in>
2026-01-16 19:39:40 +0530

    fix: URL navigating error

--- tests/e2e-pw/utils/login.js
+++ tests/e2e-pw/utils/login.js
@@ -2,7 +2,7 @@
 
 export async function login(page) {
   // Use the base URL from environment variable
-  await page.goto(`${process.env.UNOPIM_URL}`);
+  await page.goto(process.env.UNOPIM_URL);
   await page.getByRole('textbox', { name: 'Email Address' }).fill(process.env.ADMIN_USERNAME);
   await page.getByRole('textbox', { name: 'Password' }).fill(process.env.ADMIN_PASSWORD);
   await page.getByRole('button', { name: 'Sign In' }).click();
