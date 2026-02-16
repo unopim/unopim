37fa8981deab07bf774b16bac8d3680e510f1bf0
Prince <prince.sahni322@webkul.in>
2026-02-16 16:03:27 +0530

    Fix: updated changelog file with web-entrypoint.sh

--- dockerfiles/web-entrypoint.sh
+++ dockerfiles/web-entrypoint.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-set -e
 
 LOCK_FILE="/var/www/html/storage/unopim.lock"
 
 if [ ! -f "$LOCK_FILE" ]; then
-    echo "Running first-time installation..."
-
     composer install
     npm install
     php artisan unopim:install -n
-
+    
     touch "$LOCK_FILE"
-    echo "Installation completed."
-else
-    echo "UnoPim already installed. Skipping install steps."
 fi
 
-chown -R 1001:1001 /var/www/html/storage
+chown -R 1001:1001 /var/www/html
 
-exec apache2-foreground
+# Hand back control to the default entrypoint
+apache2-foreground
