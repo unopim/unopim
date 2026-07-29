4a7e647cc167ada407850e204c8709fd09dc9e87
vipinkutthi-webkul <vipinkutthi.akeneo311@webkul.in>
2026-07-29 16:12:28 +0530

    fix(admin): show the catalog breadcrumb on the association types page

--- packages/Webkul/Admin/src/Resources/views/catalog/associations/types/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/catalog/associations/types/index.blade.php
@@ -5,13 +5,13 @@
 
     {!! view_render_event('unopim.admin.catalog.association_types.index.before') !!}
 
-    <div class="flex justify-between items-center">
-        <p class="text-xl text-gray-800 dark:text-white font-bold">
-            @lang('admin::app.catalog.association_types.index.title')
-        </p>
-
-        @include('admin::catalog.associations.types.create')
-    </div>
+    <x-admin::layouts.page-header
+        :title="trans('admin::app.catalog.association_types.index.title')"
+    >
+        <x-slot:actions>
+            @include('admin::catalog.associations.types.create')
+        </x-slot>
+    </x-admin::layouts.page-header>
 
     <x-admin::datagrid :src="route('admin.catalog.association_types.index')" />
 
